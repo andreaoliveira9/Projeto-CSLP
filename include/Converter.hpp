186f46aa0c77c48f6028d6066aa6540fb5d744b8
André Oliveira <andreaoliveira@ua.pt>
2024-01-11 17:02:05 +0000

    useless things

--- include/Converter.hpp
+++ include/Converter.hpp
@@ -26,46 +26,6 @@ public:
      */
     Converter();
 
-    /** @fn rgb_to_yuv420
-     * 	@brief Converts an rgb frame to yuv420 format.
-     *
-     *	@param frame RGB frame that will be converted.
-     *	@return yuv420 frame converted from RGB.
-     */
-    Mat rgb_to_yuv420(Mat &frame);
-
-    /** @fn yuv420_to_rgb
-     * 	@brief Converts a yuv420 frame to RGB format.
-     *
-     *	@param frame yuv420 frame that will be converted.
-     *	@return RGB frame converted from yuv420.
-     */
-    Mat yuv420_to_rgb(Mat &frame);
-
-    /** @fn rgb_to_yuv422
-     * 	@brief Converts an rgb frame to yuv422 format.
-     *
-     *	@param frame RGB frame that will be converted.
-     *	@return yuv422 frame converted from RGB.
-     */
-    Mat rgb_to_yuv422(Mat &frame);
-
-    /** @fn yuv422_to_rgb
-     * 	@brief Converts a yuv422 frame to RGB format.
-     *
-     *	@param frame yuv422 frame that will be converted.
-     *	@return RGB frame converted from yuv422.
-     */
-    Mat yuv422_to_rgb(Mat &frame);
-
-    /** @fn rgb_to_yuv444
-     * 	@brief Converts an rgb frame to yuv444 format.
-     *
-     *	@param frame RGB frame that will be converted.
-     *	@return yuv444 frame converted from RGB.
-     */
-    Mat rgb_to_yuv444(Mat &frame);
-
     /** @fn yuv444_to_rgb
      * 	@brief Converts a yuv444 frame to RGB format.
      *
